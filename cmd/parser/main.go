2fbb0dd0823b388268bfa355f6d8685b83e59f1a
ifls <ifls.heyifeng@gmail.com>
2021-04-05 23:49:36 +0800

    Merge branch 'learn' of https://github.com/ifls/bolt into learn 添加注释

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
+const (
+	pageSize = 4 << 10
+)
+
 func main() {
-	dbFileName := ""
+	dbFileName := "main.db"
 	f, err := os.Open(dbFileName)
 	if err != nil {
 		log.Fatal(err)
@@ -18,17 +25,77 @@ func main() {
 		log.Fatal(err)
 	}
 
-	result, err := parse(bts)
+	db, err := parse(bts)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Fatalln(result)
+	log.Fatalln(db.String())
 }
 
-type DB struct {
+type DBInfo struct {
+	Mp [2]MetaPage
 }
 
-func parse(bts []byte) (*DB, error) {
-	return nil, nil
+func (di *DBInfo) Read(bts []byte) (int, error) {
+	_, err := di.Mp[0].Read(bts[:pageSize])
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = di.Mp[1].Read(bts[pageSize:])
+	if err != nil {
+		return 0, err
+	}
+
+	return 2 * pageSize, nil
+}
+
+func (di *DBInfo) String() string {
+	bts, err := json.MarshalIndent(di, "", " ")
+	if err != nil {
+		return "error json"
+	}
+
+	return string(bts)
+}
+
+type MetaPage struct {
+	Page page
+}
+
+func (mp *MetaPage) Read(bts []byte) (int, error) {
+	if len(bts) < 16 {
+		return 0, fmt.Errorf("len(bts) = %d", len(bts))
+	}
+	mp.Page.Id = binary.LittleEndian.Uint64(bts[:8])
+	mp.Page.Flags = binary.LittleEndian.Uint16(bts[8:10])
+	mp.Page.Count = binary.LittleEndian.Uint16(bts[10:12])
+	mp.Page.Overflow = binary.LittleEndian.Uint32(bts[12:16])
+	return 16, nil
+}
+
+func (mp *MetaPage) String() string {
+	bts, err := json.Marshal(mp)
+	if err != nil {
+		return "error MetaPage"
+	}
+
+	return string(bts)
+}
+
+type page struct {
+	Id       uint64 // 页id
+	Flags    uint16
+	Count    uint16
+	Overflow uint32
+}
+
+func parse(bts []byte) (*DBInfo, error) {
+	var db = &DBInfo{}
+	_, err := db.Read(bts)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
