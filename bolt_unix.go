63091be7039207149322575f1f30dd946914318e
ifls <ifls.heyifeng@gmail.com>
2021-06-05 14:57:46 +0800

    mmap

--- bolt_unix.go
+++ bolt_unix.go
@@ -11,6 +11,7 @@ import (
 
 // 系统调用 flock https://man7.org/linux/man-pages/man2/flock.2.html
 // flock acquires an advisory lock on a file descriptor.
+// 只是告知其他flock调用 这里有个锁， 但无法强制禁止其他进程读写文件
 func flock(db *DB, exclusive bool, timeout time.Duration) error {
 	var t time.Time
 	if timeout != 0 {
@@ -19,7 +20,7 @@ func flock(db *DB, exclusive bool, timeout time.Duration) error {
 	fd := db.file.Fd()
 	flag := syscall.LOCK_NB // non-block
 	if exclusive {
-		flag |= syscall.LOCK_EX
+		flag |= syscall.LOCK_EX // 排他
 	} else {
 		flag |= syscall.LOCK_SH // 只读的话, 加共享锁就可以
 	}
@@ -28,11 +29,11 @@ func flock(db *DB, exclusive bool, timeout time.Duration) error {
 		err := syscall.Flock(int(fd), flag)
 		if err == nil {
 			return nil
-		} else if err != syscall.EWOULDBLOCK {
+		} else if err != syscall.EWOULDBLOCK { // 如果无法完成操作，不会阻塞，而是返回此错误，这里使用循环超时，模拟阻塞
 			return err
 		}
 
-		// If we timed out then return an error.
+		// If we timed out then return an error. 不够时间下一次重试，就返回超时错误
 		if timeout != 0 && time.Since(t) > timeout-flockRetryTimeout {
 			return ErrTimeout
 		}
@@ -44,18 +45,21 @@ func flock(db *DB, exclusive bool, timeout time.Duration) error {
 
 // funlock releases an advisory lock on a file descriptor.
 func funlock(db *DB) error {
-	return syscall.Flock(int(db.file.Fd()), syscall.LOCK_UN) // 解锁
+	return syscall.Flock(int(db.file.Fd()), syscall.LOCK_UN) // 解锁标记
 }
 
 // mmap memory maps a DB's data file.
 func mmap(db *DB, sz int) error {
 	// Map the data file to memory.
+	// 保护模式，可读
+	// MAP_SHARED 共享映射， 修改对其他进程可见
 	b, err := syscall.Mmap(int(db.file.Fd()), 0, sz, syscall.PROT_READ, syscall.MAP_SHARED|db.MmapFlags)
 	if err != nil {
 		return err
 	}
 
 	// Advise the kernel that the mmap is accessed randomly.
+	// 主要是用来改善性能，建议内核不需要预读之后的几页，因为不是连续访问，不太可能访问的到
 	err = madvise(b, syscall.MADV_RANDOM)
 	if err != nil && err != syscall.ENOSYS {
 		// Ignore not implemented error in kernel because it still works.
